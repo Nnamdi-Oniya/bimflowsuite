e47edd4691c9a2f3f27fe3f1474bd403d7ef237f
Nnamdi Onyia <oniyannamdi5@gmail.com>
2026-01-26 19:49:45 +0000

    Resolved staticfiles warning at the app statrtup by removing the config from common settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -111,7 +111,6 @@ USE_TZ = True
 # Static & media
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
