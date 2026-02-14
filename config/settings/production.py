0358db7e365e10bc83bd3c6ea2bd23b8f59f5be7
Nnamdi Onyia <oniyannamdi5@gmail.com>
2026-02-14 00:13:43 +0000

    Fixed custom exception handler issue with the dev profile

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .common import *  # noqa: F403
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",") 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 SECURE_SSL_REDIRECT = True
 
@@ -13,10 +13,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://api.bimflowsuite.com",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://bimflowsuite.com",
-    "https://api.bimflowsuite.com"
-]
+CORS_ALLOWED_ORIGINS = ["https://bimflowsuite.com", "https://api.bimflowsuite.com"]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
@@ -45,38 +42,35 @@ SECURE_HSTS_PRELOAD = True
 
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-X_FRAME_OPTIONS = 'DENY'
+X_FRAME_OPTIONS = "DENY"
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 SECURE_BROWSER_XSS_FILTER = True
 
 REST_FRAMEWORK = {
-    'NON_FIELD_ERRORS_KEY': 'error',
-    'EXCEPTION_HANDLER': 'bimflow.utils.handle_exceptions.custom_exception_handler',
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "NON_FIELD_ERRORS_KEY": "error",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
     ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '3/minute',
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "3/minute",
     },
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_json_api.filters.QueryParameterValidationFilter',
-        'rest_framework_json_api.filters.OrderingFilter',
-        'rest_framework_json_api.django_filters.DjangoFilterBackend',
-        'rest_framework.filters.SearchFilter',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": (
+        "rest_framework_json_api.filters.QueryParameterValidationFilter",
+        "rest_framework_json_api.filters.OrderingFilter",
+        "rest_framework_json_api.django_filters.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
     ),
-    'SEARCH_PARAM': 'filter[search]',
+    "SEARCH_PARAM": "filter[search]",
 }
